fce3701ff25f6c91d350466ae9377f1f3a83a3a0
AlexIstra1989 <alex888fcsm@gmail.com>
2022-10-13 22:03:36 +0300

    fixed catalog-page

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -263,8 +263,8 @@ function initSliders() {
 			modules: [Navigation, Pagination],
 			observer: true,
 			observeParents: true,
-			slidesPerView: 1,
-			spaceBetween: 0,
+			slidesPerView: 7,
+			spaceBetween: 10,
 			autoHeight: true,
 			speed: 800,
 
@@ -309,6 +309,86 @@ function initSliders() {
 			
 			breakpoints: {
 				320: {
+					slidesPerView: 2,
+					spaceBetween: 0,
+					autoHeight: true,
+				},
+    500: {
+				 slidesPerView: 3,
+				 spaceBetween: 2,
+				 autoHeight: true,
+				},
+    620: {
+				 slidesPerView: 4,
+				 spaceBetween: 2,
+				 autoHeight: true,
+				},
+    755: {
+				 slidesPerView: 5,
+				 spaceBetween: 2,
+				 autoHeight: true,
+				},
+			},
+			
+			// События
+			on: {
+
+			}
+		});
+	};
+ if (document.querySelector('.more__slider ')) { // Указываем скласс нужного слайдера
+		// Создаем слайдер
+		new Swiper('.more__slider ', { // Указываем скласс нужного слайдера
+			// Подключаем модули слайдера
+			// для конкретного случая
+			modules: [Navigation, Pagination],
+			observer: true,
+			observeParents: true,
+			slidesPerView: 1,
+			spaceBetween: 0,
+			autoHeight: true,
+			speed: 800,
+
+			//touchRatio: 0,
+			//simulateTouch: false,
+			//loop: true,
+			//preloadImages: false,
+			//lazy: true,
+
+			/*
+			// Эффекты
+			effect: 'fade',
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+			*/
+
+			// Пагинация
+			
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
+			
+
+			// Скроллбар
+			/*
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true,
+			},
+			*/
+
+			// Кнопки "влево/вправо"
+			navigation: {
+				prevEl: '.swiper-button-prev',
+				nextEl: '.swiper-button-next',
+			},
+
+			// Брейкпоинты
+				breakpoints: {
+				320: {
 					slidesPerView: 1,
 					spaceBetween: 5,
 					autoHeight: true,
@@ -323,7 +403,6 @@ function initSliders() {
 				 spaceBetween: 20,
 				}
 			},
-			
 			// События
 			on: {
 
@@ -331,7 +410,6 @@ function initSliders() {
 		});
 	};
 }
-
 
 // Скролл на базе слайдера (по классу swiper_scroll для оболочки слайдера)
 function initSlidersScroll() {
